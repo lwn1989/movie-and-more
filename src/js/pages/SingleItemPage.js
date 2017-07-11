6014f3262c842cbcb114c28b2977969703d5d55b
lwn1989 <weinanxli@gmail.com>
2017-07-11 21:44:13 +0200

    finish single item page, fixed errors

--- src/js/pages/SingleItemPage.js
+++ src/js/pages/SingleItemPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import SingleItemStore from '../data/SingleItemStore.js'
 import movieStore from '../data/MovieStore.js'
 import tvStore from '../data/TvStore.js'
+import MainInfo from '../components/movie/MainInfo.js'
 import { observer } from 'mobx-react'
 import { SingleSlider } from '../components/movie/MovieSlider.js'
 
@@ -13,6 +14,14 @@ export default class SingleItemPage extends React.Component {
     this.mediaType = (props.match.url[1] === 'm') ? 'mov' : 'tv'
     this.itemStore = new SingleItemStore({ 'itemId': this.itemId, 'mediaType': this.mediaType })
     this.itemStore.getItemInfo()
+    this.state = {itemId: this.itemId}
+  }
+  componentWillReceiveProps (nextProps) {
+    this.itemId = nextProps.match.params.itemId
+    this.mediaType = (nextProps.match.url[1] === 'm') ? 'mov' : 'tv'
+    this.itemStore = new SingleItemStore({ 'itemId': this.itemId, 'mediaType': this.mediaType })
+    this.itemStore.getItemInfo()
+    this.state = {itemId: this.itemId}
   }
 
   checkObjEmpty (obj) {
@@ -29,11 +38,12 @@ export default class SingleItemPage extends React.Component {
       var status = this.itemStore.pendingRequests > 0 ? <marquee direction='right'>Loading...</marquee> : null
       return status
     } else {
-      console.log(fullInfo)
       return (
         <div className='singleItemPage'>
           { this.mediaType === 'mov' ? <SingleSlider mediaType={this.mediaType} store={movieStore} singleSlide movieInfo={fullInfo} />
           : <SingleSlider mediaType={this.mediaType} store={tvStore} singleSlide movieInfo={fullInfo} />}
+          { this.mediaType === 'mov' ? <MainInfo mediaType={this.mediaType} movieInfo={fullInfo} />
+          : <MainInfo mediaType={this.mediaType} movieInfo={fullInfo} />}
         </div>
       )
     }
